986c5eab8a02b0a4832a15b59b2d8060eeef1ede
Edmond O'Flynn <oflynned@tcd.ie>
2019-08-18 23:37:54 +0200

    add debug process env

--- src/common/http.js
+++ src/common/http.js
@@ -1,3 +1,3 @@
 export const fetchResource = (url, options) =>
-  fetch(process.env.REACT_APP_ENVIRONMENT === "production" ?
+  fetch(process.env.GATSBY_ENVIRONMENT === "production" ?
     "https://api.stocket.io" + url : "http://localhost:3000" + url, options)
