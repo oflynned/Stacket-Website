8dec289db5462f9294c91fb35800c8cceadfc275
Edmond O'Flynn <oflynned@tcd.ie>
2019-08-18 20:55:22 +0200

    force rebuild

--- src/common/http.js
+++ src/common/http.js
@@ -1,4 +1,3 @@
-export const fetchResource = (url, options) => {
-  return fetch(process.env.REACT_APP_ENVIRONMENT === "production" ?
+export const fetchResource = (url, options) =>
+  fetch(process.env.REACT_APP_ENVIRONMENT === "production" ?
     "https://api.stocket.io" + url : "http://localhost:3000" + url, options)
-}
